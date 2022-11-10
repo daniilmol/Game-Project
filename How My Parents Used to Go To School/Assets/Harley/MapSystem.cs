6ac07b76b0a492a9250c25d5ab686c73001e972f
Harley Guan <guardhao@outlook.com>
2022-11-10 12:18:33 -0800

    Add cross builder

--- How My Parents Used to Go To School/Assets/Harley/MapSystem.cs
+++ How My Parents Used to Go To School/Assets/Harley/MapSystem.cs
@@ -6,13 +6,36 @@ public class MapSystem : MonoBehaviour
 { 
     public MapData mapData;
     public RoomBuilder roomBuilder;
+    public CrossBuilder crossBuilder;
 
     public List<RoomTran> genRooms = new List<RoomTran>();
     public List<RoomTran> unCrossRooms = new List<RoomTran>();
 
+    private RoomTran firstRoom;
+    private RoomTran lastRoom;
+
+    public float anotherCrossProbability = .2f;
+    public float deadCrossProbability = .5f;
+    public float deadAwayProbability = .6f;
+
+    List<RoomTran> deadRooms = new List<RoomTran>();
+    Dictionary<RoomTran, List<RoomTran>> roomCrossRooms = new Dictionary<RoomTran, List<RoomTran>>();
+
+    // The main cross of map
+    public LinkedList<RoomTran> mainCross = new LinkedList<RoomTran>();
+
+    List<RoomTran> endRooms = new List<RoomTran>();
+
+    [HideInInspector]
+    public List<GameObject> crossUnitInsts = new List<GameObject>();
+    [HideInInspector]
+    public List<GameObject> roomUnitInsts = new List<GameObject>();
+
+    const string cellTag = "Floor";
+
     public void Start()
     {
-        // CrossBuilder = GetComponent<CrossBuilder>();
+        crossBuilder = GetComponent<CrossBuilder>();
         roomBuilder = GetComponent<RoomBuilder>();
     }
 
@@ -55,7 +78,7 @@ public class MapSystem : MonoBehaviour
             rd.roomTran = genRooms[i - 1];
             rd.roomType = RoomData.RoomType.QuizRoom;
             if (rd.roomId == 1)
-                rd.roomType = RoomData.RoomType.FinalRoom;
+                rd.roomType = RoomData.RoomType.AssignmentRoom;
             rd.crossRooms = new List<RoomData>();
             rd.monsters = new List<GameObject>();
             rd.isEndRoom = false;
@@ -68,6 +91,327 @@ public class MapSystem : MonoBehaviour
 
     void RandRoomCrosses()
     {
+        if (genRooms.Count <= 0) return;
 
+        firstRoom = genRooms[Random.Range(0, genRooms.Count)];
+
+        CalNextCross(firstRoom);
     }
+
+    void UpdateMapData()
+    {
+        foreach (var rd in mapData.roomDataDic)
+        {
+            RoomTran rt = rd.Value.roomTran;
+            if (roomCrossRooms.ContainsKey(rt))
+            {
+                var temp = new List<int>();
+                foreach (var crt in roomCrossRooms[rt])
+                {
+                    var id = GetRoomIdByRT(crt);
+                    if (id > 0)
+                    {
+                        temp.Add(id);
+
+                        rd.Value.crossRooms.Add(mapData.roomDataDic[id]);
+
+                        var cd = new CrossData();
+                        cd.id1 = rd.Key;
+                        cd.id2 = id;
+                        if (!CrossDataContains(cd))
+                            mapData.crossDataList.Add(cd);
+                    }
+                }
+                mapData.roomCrossRoomsDic.Add(rd.Key, temp);
+            }
+
+            if (mainCross.Contains(rt))
+            {
+                rd.Value.isMainCrossRoom = true;
+                if (endRooms.Contains(rt))
+                {
+                    rd.Value.roomType = RoomData.RoomType.FinalRoom;
+                }
+            }
+
+            if (endRooms.Contains(rt))
+            {
+                rd.Value.isEndRoom = true;
+            }
+        }
+    }
+
+    bool CrossDataContains(CrossData d)
+    {
+        foreach (var cd in mapData.crossDataList)
+        {
+            if ((cd.id1 == d.id1 && cd.id2 == d.id2) || (cd.id1 == d.id2 && cd.id2 == d.id1))
+                return true;
+        }
+        return false;
+    }
+
+    int GetRoomIdByRT(RoomTran rt)
+    {
+        foreach (var rd in mapData.roomDataDic)
+        {
+            if (rd.Value.roomTran == rt)
+                return rd.Key;
+        }
+        return -1;
+    }
+
+    void CalNextCross(RoomTran nr, RoomTran br = null)
+    {
+        mainCross.AddLast(nr);
+        LRemove(unCrossRooms, nr);
+
+        var fcl = FindLatelyRooms(nr);
+
+        if (fcl.firstLately != null)
+        {
+            //CrossGenCtrl.CrossTwoRoom(nr, fcl.FirstLately);
+            //UnCrossRooms.Remove(fcl.FirstLately);
+
+            if (fcl.secondLately != null)
+            {
+                if (Random.value < anotherCrossProbability)
+                {
+                    //CrossGenCtrl.CrossTwoRoom(nr, fcl.SecondLately);
+                    //UnCrossRooms.Remove(fcl.SecondLately);
+
+                    if (Random.value < deadCrossProbability)
+                    {
+                        var dr = Random.value < .5f ? fcl.firstLately : fcl.secondLately;
+                        var lr = dr == fcl.firstLately ? fcl.secondLately : fcl.firstLately;
+
+                        //DeadRooms.Add(dr);
+                        LAdd(deadRooms, dr);
+
+                        //RoomCrossRooms.Add(nr, new List<RoomTran>() { lr });
+                        AddItemToRoomDic(nr, lr, dr, br);
+                        //MainCross.AddLast(lr);
+                        LRemove(unCrossRooms, dr);
+                        LRemove(unCrossRooms, lr);
+
+                        CalDeadCross(dr, nr);
+                        CalNextCross(lr, nr);
+                    }
+                    else
+                    {
+                        var mr = Random.value < .5f ? fcl.firstLately : fcl.secondLately;
+                        var or = mr == fcl.firstLately ? fcl.secondLately : fcl.firstLately;
+                        AddItemToRoomDic(or, nr);
+                        AddItemToRoomDic(nr, mr, or, br);
+                        CalNextCross(mr, nr);
+                    }
+                }
+                else
+                {
+                    AddItemToRoomDic(nr, br, fcl.firstLately);
+                    CalNextCross(fcl.firstLately, nr);
+                }
+            }
+            else
+            {
+                AddItemToRoomDic(nr, br, fcl.firstLately);
+                CalNextCross(fcl.firstLately, nr);
+            }
+        }
+        else
+        {
+            lastRoom = nr;
+            AddItemToRoomDic(nr, br);
+            LAdd(endRooms, nr);
+
+            Debug.Log("Generate Room Number£º" + genRooms.Count);
+            Debug.Log("Main Cross Number£º" + mainCross.Count);
+            Debug.Log("Dead Rooms Number£º" + deadRooms.Count);
+            Debug.Log("End Rooms Number£º" + endRooms.Count);
+
+            UpdateMapData();
+            crossBuilder.StartCoroutine(crossBuilder.GenCrosses());
+        }
+    }
+
+    CrossLately FindLatelyRooms(RoomTran tar)
+    {
+        var cl = new CrossLately();
+        float firstSqrdis = Mathf.Infinity;
+        float secondSqrdis = Mathf.Infinity;
+
+        foreach (var room in unCrossRooms)
+        {
+            var rc = new Vector3(room.centerPos.x, 0, room.centerPos.y);
+            var tc = new Vector3(tar.centerPos.x, 0, tar.centerPos.y);
+            float sqrdis = (rc - tc).sqrMagnitude;
+
+            if (sqrdis < firstSqrdis)
+            {
+                firstSqrdis = sqrdis;
+                cl.firstLately = room;
+            }
+            else if (sqrdis < secondSqrdis)
+            {
+                secondSqrdis = sqrdis;
+                cl.secondLately = room;
+            }
+        }
+        return cl;
+    }
+
+    void CalDeadCross(RoomTran fdr, RoomTran bdr)
+    {
+        var temp = FindLatelyRooms(fdr);
+        if (temp.firstLately != null && Random.value < deadAwayProbability)
+        {
+            //CrossGenCtrl.CrossTwoRoom(fdr, temp.FirstLately);
+            LRemove(unCrossRooms, temp.firstLately);
+            LAdd(deadRooms, temp.firstLately);
+            AddItemToRoomDic(fdr, temp.firstLately, bdr);
+            CalDeadCross(temp.firstLately, fdr);
+        }
+        else
+        {
+            // Dead the cross
+            LRemove(unCrossRooms, fdr);
+            LAdd(deadRooms, fdr);
+            AddItemToRoomDic(fdr, bdr);
+            LAdd(endRooms, fdr);
+        }
+    }
+
+    void LRemove<T>(List<T> list, T item)
+    {
+        if (list.Contains(item))
+        {
+            list.Remove(item);
+        }
+    }
+
+    void LAdd<T>(List<T> list, T item)
+    {
+        if (!list.Contains(item))
+        {
+            list.Add(item);
+        }
+    }
+
+    void AddItemToRoomDic(RoomTran key, RoomTran item1, RoomTran item2 = null, RoomTran item3 = null)
+    {
+        if (roomCrossRooms.ContainsKey(key))
+        {
+            if (item1 != null)
+                if (!roomCrossRooms[key].Contains(item1))
+                    roomCrossRooms[key].Add(item1);
+            if (item2 != null)
+                if (!roomCrossRooms[key].Contains(item2))
+                    roomCrossRooms[key].Add(item2);
+            if (item3 != null)
+                if (!roomCrossRooms[key].Contains(item3))
+                    roomCrossRooms[key].Add(item3);
+        }
+        else
+        {
+            roomCrossRooms.Add(key, new List<RoomTran>());
+            AddItemToRoomDic(key, item1, item2, item3);
+        }
+    }
+
+    public bool RayCast(Vector3 ori, Vector3 dir, float mD)
+    {
+        Ray ray = new Ray(ori, dir);
+        RaycastHit info;
+        if (Physics.Raycast(ray, out info, mD))
+        {
+            if (info.transform.tag == cellTag)
+                return true;
+        }
+        return false;
+    }
+
+    public GameObject InsSetPos(GameObject prefab, Vector3 pos, Transform parent = null)
+    {
+        var ins = Instantiate(prefab);
+        ins.transform.position = pos;
+        ins.transform.parent = parent;
+        return ins;
+    }
+
+    public RoomTran GetRoomByPos(Vector3 pos)
+    {
+        foreach (var room in genRooms)
+        {
+            var to = new Vector3(room.length, 0, room.width) * .5f;
+
+            var centerPos = new Vector3(room.centerPos.x, 0, room.centerPos.y);
+            var ned = centerPos - to;
+            var fod = centerPos + to;
+
+            if (pos.x >= ned.x && pos.x <= fod.x && pos.y >= ned.y && pos.y <= fod.y && pos.z >= ned.z && pos.z <= fod.z)
+            {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public ExCross GetExCross(Vector3 strPos, Vector3 tarPos)
+    {
+        var ec = new ExCross();
+        var rt = GetRoomByPos(strPos);
+        if (rt != null)
+        {
+            var to = new Vector3(rt.length - 1, 0, rt.width - 1) * .5f;
+
+            var centerPos = new Vector3(rt.centerPos.x, 0, rt.centerPos.y);
+            var ned = centerPos - to;
+            var fod = centerPos + to;
+            if (strPos.x == tarPos.x)
+            {
+                if (Random.value < .5f)
+                {
+                    ec.pos1 = strPos.z < tarPos.z ? new Vector3(ned.x - 1, strPos.y, strPos.z - 1) : new Vector3(ned.x - 1, strPos.y, strPos.z + 1);
+                    ec.pos2 = strPos.z < tarPos.z ? ec.pos1 + new Vector3(0, 0, rt.width + 1) : ec.pos1 - new Vector3(0, 0, rt.width + 1);
+                    ec.pos3 = new Vector3(strPos.x, strPos.y, ec.pos2.z);
+                }
+                else
+                {
+                    ec.pos1 = strPos.z < tarPos.z ? new Vector3(fod.x + 1, strPos.y, strPos.z - 1) : new Vector3(fod.x + 1, strPos.y, strPos.z + 1);
+                    ec.pos2 = strPos.z < tarPos.z ? ec.pos1 + new Vector3(0, 0, rt.width + 1) : ec.pos1 - new Vector3(0, 0, rt.width + 1);
+                    ec.pos3 = new Vector3(strPos.x, strPos.y, ec.pos2.z);
+                }
+
+            }
+            else if (strPos.z == tarPos.z)
+            {
+                if (Random.value < .5f)
+                {
+                    ec.pos1 = strPos.x < tarPos.x ? new Vector3(strPos.x - 1, strPos.y, ned.z - 1) : new Vector3(strPos.x + 1, strPos.y, ned.z - 1);
+                    ec.pos2 = strPos.x < tarPos.x ? ec.pos1 + new Vector3(rt.length + 1, 0, 0) : ec.pos1 - new Vector3(rt.length + 1, 0, 0);
+                    ec.pos3 = new Vector3(ec.pos2.x, strPos.y, strPos.z);
+                }
+                else
+                {
+                    ec.pos1 = strPos.x < tarPos.x ? new Vector3(strPos.x - 1, strPos.y, fod.z + 1) : new Vector3(strPos.x + 1, strPos.y, fod.z + 1);
+                    ec.pos2 = strPos.x < tarPos.x ? ec.pos1 + new Vector3(rt.length + 1, 0, 0) : ec.pos1 - new Vector3(rt.length + 1, 0, 0);
+                    ec.pos3 = new Vector3(ec.pos2.x, strPos.y, strPos.z);
+                }
+            }
+        }
+        return ec;
+    }
+}
+
+public struct CrossLately
+{
+    public RoomTran firstLately;
+    public RoomTran secondLately;
+}
+
+public struct ExCross
+{
+    public Vector3 pos1;
+    public Vector3 pos2;
+    public Vector3 pos3;
 }
