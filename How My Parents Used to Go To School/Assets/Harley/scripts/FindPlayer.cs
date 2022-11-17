9b5cb468bab151b567c87acfe5535f5ed706f51a
Harley Guan <guardhao@outlook.com>
2022-11-17 02:50:03 -0800

    Switch scene works but cannot change position

--- How My Parents Used to Go To School/Assets/Harley/scripts/FindPlayer.cs
+++ How My Parents Used to Go To School/Assets/Harley/scripts/FindPlayer.cs
@@ -7,7 +7,7 @@ public class FindPlayer : MonoBehaviour
     public GameObject bulletPrefab;
 
     private void Awake() {
-        var player = GameObject.Find("Male_C");
+        var player = GameObject.Find("Male C");
         this.GetComponent<Enemy>().SetTarget(player, bulletPrefab);
         // this.GetComponent<EnemyStatContainer>().IncreaseStats(scale);
         // this.GetComponent<EnemyStatContainer>().IncreaseStats(PlayerPrefs.GetFloat("Scale"));
